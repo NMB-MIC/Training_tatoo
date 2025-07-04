444c4b6c732b87dcaa85413e24b0256541abf2e8
hztm999 <t0925292044@gmail.com>
2025-07-04 11:24:02 +0700

    123

--- client/src/page/Login.js
+++ client/src/page/Login.js
@@ -23,7 +23,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://192.168.100.124:3001/login', {
+      const res = await fetch('http://192.168.100.124:3001/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
